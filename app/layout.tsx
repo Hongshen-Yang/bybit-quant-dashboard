37a6f1b3cf68f6ef7858eff52ca33e51156a83ff
yanghongshen <hello@hongshen.me>
2026-04-27 20:03:29 +1200

    vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { AutoRefresh } from "./components/AutoRefresh";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -31,6 +33,8 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <AutoRefresh />
         {children}
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
